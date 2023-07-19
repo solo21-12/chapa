f544879c68db97da10410009e87c75e9ef63afa0
solo2112 <dawitabrham0021@gmail.com>
2023-07-19 13:04:52 +0300

    card section improvement

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,84 +15,79 @@ type Props = {
 
 export default function Navbar({ setToggle, toggle }: Props) {
   return (
-    <motion.div
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 2,
-        type: "tween",
-      }}
-      className=" sm:w-[90%] h-[32px] flex-shrink-0 flex py-10 justify-between items-center navbar sm:px-10 px-4 z-10"
-    >
-      <div>
-        <motion.img
-          initial={{
-            x: -100,
-            opacity: 0,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-          }}
-          transition={{
-            duration: 2,
-          }}
-          src="/chapa1.png"
-          alt="chapa logo"
-          className="h-14 w-14"
-        />
-      </div>
-      <ul className="  list-none sm:flex hidden justify-end items-center flex-1 gap-5 font-poppins z-[200]">
+    <>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 2,
+          type: "tween",
+        }}
+        className=" sm:w-[90%] h-[32px] flex-shrink-0 flex py-10 justify-between items-center navbar sm:px-10 px-4 z-10"
+      >
+        <div>
+          <motion.img
+            initial={{
+              x: -100,
+              opacity: 0,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+            }}
+            transition={{
+              duration: 2,
+            }}
+            src="/chapa1.png"
+            alt="chapa logo"
+            className="h-14 w-14"
+          />
+        </div>
+        <ul className="  list-none sm:flex hidden justify-end items-center flex-1 gap-5 font-poppins z-[200]">
+          {navLinks.map((item, i) => (
+            <li
+              key={i}
+              className={`font-poppins cursor-pointer text-[16px] hover:text-green-400  font-[400]  leading-[130%]`}
+            >
+              <a href={`#${item.id}`}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className={` sm:hidden flex flex-1 justify-end text-3xl  `}>
+          <div className="">
+            <div className=" slide-bottom text-green-400">
+              {!toggle ? (
+                <BiMenuAltRight
+                  fontSize={27}
+                  className="overlay__close  text-4xl"
+                  onClick={() => setToggle(!toggle)}
+                />
+              ) : (
+                <AiOutlineClose
+                  fontSize={27}
+                  className=" text-4xl z-50 text-green-400 "
+                  onClick={() => setToggle(!toggle)}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <ul className={` ${toggle ? " flex flex-col h-[90vh] justify-center items-center align-middle sidebar gap-5" : "hidden"}`}>
         {navLinks.map((item, i) => (
           <li
             key={i}
-            className={`font-poppins cursor-pointer text-[16px] hover:text-green-400  font-[400]  leading-[130%]`}
+            className={`font-poppins cursor-pointer hover:text-slate-100  font-[500]  leading-[130%] text-2xl ${styles.GreenGradientText}`}
           >
-            <a href={`#${item.id}`}>{item.title}</a>
+            <Link href={`#${item.id}`}>{item.title}</Link>
           </li>
         ))}
       </ul>
-
-      <div className={` sm:hidden flex flex-1 justify-end text-3xl  `}>
-        <div className="app__navbar-smallscreen">
-          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom text-green-400">
-            {!toggle ? (
-              <BiMenuAltRight
-                fontSize={27}
-                className="overlay__close  text-4xl"
-                onClick={() => setToggle(!toggle)}
-              />
-            ) : (
-              <AiOutlineClose
-                fontSize={27}
-                className="overlay__close text-4xl z-50 text-green-400 "
-                onClick={() => setToggle(!toggle)}
-              />
-            )}
-
-            <ul
-              className={` ${
-                toggle
-                  ? "flex app__navbar-smallscreen-links align-middle items-center justify-center gap-5  flex-col h-screen font-poppins sidebar"
-                  : "hidden"
-              }`}
-            >
-              {navLinks.map((item, i) => (
-                <li
-                  key={i}
-                  className={`font-poppins cursor-pointer hover:text-slate-100  font-[500]  leading-[130%] text-2xl ${styles.GreenGradientText}`}
-                >
-                  <Link href={`#${item.id}`}>{item.title}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </motion.div>
+    </>
   );
 }
